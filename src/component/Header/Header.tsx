b8502b081ef724c05265afce2c98e473cbeca5b8
mateociric <mciricprogramming@gmail.com>
2023-06-28 11:21:29 +0200

    setting up project, creating Header component & style folder, continue with Header

---
+++ src/component/Header/Header.tsx
@@ -0,0 +1,12 @@
+import 'component/Header/Header.scss';
+
+function Header() {
+
+    return(
+        <nav>
+            <a href="">HOMEPAGE</a>
+            <a href="">FAVORITE VEHICLE</a>
+            <a href="">ABOUT APP</a>
+        </nav>
+    ) 
+}
