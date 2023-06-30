1739a43e2679939e0886d39d4f10697f581ee582
mateociric <mciricprogramming@gmail.com>
2023-06-30 20:45:47 +0200

    work on menu component, continue with select tag

--- src/component/Menu/Menu.tsx
+++ src/component/Menu/Menu.tsx
@@ -1,15 +1,31 @@
+import 'component/Menu/Menu.scss'
+
 function Menu() {
     return (
         <>
-            <section className="title">
-                <h1>VEHICLE APP</h1>
-            </section>
-            <section className="search">
-            </section>
-            <section className="sort">
-            </section>
-            <section className="create">
-            </section>
+            <div className='menu'>
+                <section>
+                    <p>MENU</p>
+                </section>
+                <section>
+                    <label>search vehicle</label>
+                    <input type="text" />
+                </section>
+                <section>
+                    <label>sort vehicle</label>
+                    <input type="text" />
+                </section>
+                <section>
+                    <label>create new vehicle</label>
+                    <select name="" id="">
+                        <option value="Mercedes-Benz">Mercedes-Benz</option>
+                        <option value="BMW">BMW</option>
+                    </select>
+                    <select name="" id="">
+                        
+                    </select>
+                </section>
+            </div>
         </>
     )
 }
